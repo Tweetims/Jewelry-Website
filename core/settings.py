0a804115faacc44a4cf88381c2e626c6d6e273a3
Tweetims <cclchris1101@gmail.com>
2022-07-09 20:00:19 -0600

    Heroku fix

--- core/settings.py
+++ core/settings.py
@@ -145,6 +145,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(CORE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(CORE_DIR, 'mediafiles')
+MEDIA_URL = '/media/'
+
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(CORE_DIR, 'apps/static'),
--- core/urls.py
+++ core/urls.py
@@ -2,13 +2,17 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
-
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include  # add this
+from django.views.static import serve
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
     path("", include("apps.authentication.urls")), # Auth routes - login / register
     path("", include("apps.videos.urls")),
-    path("", include("apps.home.urls"))
+    path("", include("apps.home.urls")),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
