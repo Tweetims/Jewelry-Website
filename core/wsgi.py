3a2a186509e21969511673e59b5e3788a3ac0028
Tweetims <cclchris1101@gmail.com>
2022-07-09 20:08:47 -0600

    whitenoise fix

--- core/wsgi.py
+++ core/wsgi.py
@@ -6,7 +6,10 @@ Copyright (c) 2019 - present AppSeed.us
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root='./staticfiles')
+
