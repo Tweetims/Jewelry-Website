88b41a4094912039697f87830a278e4d0b34a6c5
Tweetims <cclchris1101@gmail.com>
2022-07-09 20:14:46 -0600

    whitenoise fix

--- core/wsgi.py
+++ core/wsgi.py
@@ -11,5 +11,5 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='./staticfiles')
+application = WhiteNoise(application, root='./apps/static')
 
