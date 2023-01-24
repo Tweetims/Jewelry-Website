bda2b0b53c9a4b08c96e916c6711c353f50763e8
Tweetims <cclchris1101@gmail.com>
2023-01-23 19:03:43 -0700

    Move static files, fix static file ref

--- core/settings.py
+++ core/settings.py
@@ -138,11 +138,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "apps/static/"),
-)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
-STATIC_ROOT = os.path.join(BASE_DIR, "apps/static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
